6b4d76795efe9e75ea4254c377de848eed48a661
bangeravivek <bangeravivek@f7ce5938-7109-4973-91c0-8e717d788be3>
2010-09-26 17:34:42 +0000

    git-svn-id: https://conms.svn.sourceforge.net/svnroot/conms@6 f7ce5938-7109-4973-91c0-8e717d788be3

---
+++ system/application/models/Payment_model.php
@@ -0,0 +1,47 @@
+<?php
+
+	class Payment_model extends Model{
+		$table='payment';
+		function Payment_model(){
+			parent::Model();
+		}
+
+		function make_payment($data)
+		{
+			$query=$this->db->get_where($table,$data);
+			if($query->num_rows>0)
+			{
+				echo('Payment already made');
+				reurn FALSE;
+			}
+			else
+			{
+				$this->db->insert($table,$data);
+				return TRUE;
+			}
+		}
+	
+		function get_payment($data)
+		{
+			$query=$this->db->get_where($table,$data);
+			if($query->num_rows()==0)
+				return FALSE;
+			else
+				return ($query->result() as $row);
+		}
+		
+		function findrem($data)
+		{
+			$query=$this->db->get_where($table,$data);
+			return ($query->result() as $row);
+		}
+
+		function payment_status($data){
+			$this->db->select('Payment_status');
+			$query=$this->db->where($table,$data);
+			return ($query->result() as $row);
+		}
+
+
+?>
+		
