fc1f8579f7bf6d6c243fc726ad91a9486b137e2b
natang <natang@f7ce5938-7109-4973-91c0-8e717d788be3>
2010-10-06 02:14:55 +0000

    git-svn-id: https://conms.svn.sourceforge.net/svnroot/conms@32 f7ce5938-7109-4973-91c0-8e717d788be3

--- system/application/models/track_model.php
+++ system/application/models/track_model.php
@@ -5,16 +5,21 @@
 			parent::Model();
 		}
 
-        function get_details($id){
+		 function list_tracks(){
+            		 $query = $this->db->query("SELECT * FROM ".$this->_table);
+            		 return $query->result_array();
+        		}
+
+        		function get_details($id){
             		$query = $this->db->get_where($this->_table,array('id'=>$id));
             		return ($query->row_array();)
 		}
 
 		funtion get_papers($id){
 		$CI =& get_instance();
-                $CI->load->model('paper_model');
-		        $query=$CI->paper_model->paper_status(array('track_id'=$id));
-                $this->paper_model = $CI->paper_model;
+                	$CI->load->model('paper_model');
+		$query=$CI->paper_model->paper_status(array('track_id'=$id));
+                	$this->paper_model = $CI->paper_model;
 		return $query->row_array();
 		}
 
