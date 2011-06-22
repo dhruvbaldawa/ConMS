5d1f9a25ea8e380fed7067008a60f6d26841044c
Natang Salgia <nasa120890-mail@yahoo.com>
2011-06-22 22:39:05 +0530

    just added privileges for review form.

--- application/views/site/papers.php
+++ application/views/site/papers.php
@@ -355,7 +355,15 @@ function getCSVData(){
                                 <td>
 									<a href="#" class="update_paper" id="<?php echo $arow['id']; ?>" rel="<?php echo base_url(); ?>papers/view/paper_update_form"><img src="<?php echo base_url(); ?>images/ico_edit_16.png" class="icon16 fl-space2" alt="" title="edit" /></a>
 									<a href="#"><img src="<?php echo base_url(); ?>images/ico_delete_16.png" class="icon16 fl-space2" alt="" title="delete" /></a>
-                                    <a href="#" class="assign_reviewers" id="<?php echo $arow['id']; ?>" rel="<?php echo base_url(); ?>papers/view/review_form"><img src="<?php echo base_url(); ?>images/ico_attention_16.png" class="icon16 fl-space2" alt="" title="assign reviwers" /></a>
+		<?php
+		if($this->auth_model->is_chairperson() || $this->auth_model->is_admin() || $this->auth_model->is_manager()){?>
+
+                                    <a href="#" class="assign_reviewers" id="<?php echo $arow['id']; ?>" rel="<?php echo base_url(); ?>papers/view/reviewer_form"><img src="<?php echo base_url(); ?>images/ico_attention_16.png" class="icon16 fl-space2" alt="" title="assign reviwers" /></a>
+
+		<?php } else if($this->auth_model->is_reviewer()){?>
+
+		     <a href="#" class="assign_reviewers" id="<?php echo $arow['id']; ?>" rel="<?php echo base_url(); ?>papers/view/review_form"><img src="<?php echo base_url(); ?>images/ico_attention_16.png" class="icon16 fl-space2" alt="" title="assign reviwers" /></a>
+		<?php }?>	
 
 								</td>
 							</tr>
